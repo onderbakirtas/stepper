5b7ef46269f12ca9f85512ecccedb9719aff8a4a
Onder Bakirtas <onderbakirtas@gmail.com>
2022-10-19 14:53:06 +0300

    feat: add class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+type TCountdownOptions = {
+  start: number
+}
+
+class Countdown {
+  constructor(node: HTMLElement, options: TCountdownOptions) {
+    console.log(node)
+    console.log('options:', options)
+    // this.init(options.start)
+  }
+
+  // init(target: number) {
+  //   let timeleft = target
+  //   const timer = setInterval(() => {
+  //     if (timeleft === 0) {
+  //       clearInterval(timer)
+  //     }
+  //     timeleft -= -1
+  //   })
+  // }
+}
+
+export { Countdown }
+
 export const Stepper = (step: number) => `Current step is: ${step}`
